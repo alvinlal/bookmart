ca6e4d07a838bb22c0f787db5275979d35516f33
Alvin Lal <alvinzzz2001@gmail.com>
2021-07-03 23:42:37 +0530

    ✨ finished home-items-section

--- index.php
+++ index.php
@@ -19,6 +19,171 @@
 <div class="home-page-circle-3"></div>
 
 <section class="home-items-section">
+    <a href="" class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/azk.jpg" />
+        <div class="book-preview-title-author">
+            <h1>Harry Potter And the Prisoner of Azkaban</h1>
+            <p>By <span>J.K Rowling</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹332</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/and.jpg" />
+        <div class="book-preview-title-author">
+            <h1>And Then There Were None</h1>
+            <p>By <span>Agatha Christie</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹181</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/pap.jpg" />
+        <div class="book-preview-title-author">
+            <h1>Pride And Prejudice</h1>
+            <p>By <span>Jane Austen</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹425</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/breathe.jpg" />
+        <div class="book-preview-title-author">
+            <h1>When Breath Becomes Air</h1>
+            <p>By <span>Paul Kalanithi</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹361</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/sherlock.jpg" />
+        <div class="book-preview-title-author">
+            <h1>Sherlock Holmes - A Study In Pink</h1>
+            <p>By <span>Authur Conan Doyle</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹455</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/alchemist.jpg" />
+        <div class="book-preview-title-author">
+            <h1>The Alchemist</h1>
+            <p>By <span>Paulo Coelho</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹75</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/ikigai.jpg" />
+        <div class="book-preview-title-author">
+            <h1>Ikigai: The Japanese secret to a long and happy life</h1>
+            <p>By <span>Hector Garcia</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹299</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/moonwalker.jpg" />
+        <div class="book-preview-title-author">
+            <h1>Moonwalk</h1>
+            <p>By <span>Micheal Jackson</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹455</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/sirapj.jfif" />
+        <div class="book-preview-title-author">
+            <h1>Wings Of Fire</h1>
+            <p>By <span>Dr APJ Abdul Kalam</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹169</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/theranos.jpg" />
+        <div class="book-preview-title-author">
+            <h1>Bad Blood: Secrets and Lies in a Silicon Valley Startup</h1>
+            <p>By <span>John Carreyrou</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹300</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/ffa.jpg" />
+        <div class="book-preview-title-author">
+            <h1>Five Feet Apart</h1>
+            <p>By <span>Rachael Lippincott</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹799</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/davinci.jpg" />
+        <div class="book-preview-title-author">
+            <h1>The Da Vinci Code</h1>
+            <p>By <span>Dan Brown</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹299</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/had.jpg" />
+        <div class="book-preview-title-author">
+            <h1>Harry Potter And The Deathly Hallows</h1>
+            <p>By <span>J.K Rowling</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹455</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/wimpy.jpg" />
+        <div class="book-preview-title-author">
+            <h1>Diary of a Wimpy Kid: Rodrick Rules</h1>
+            <p>By <span>Jeff Kinney</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹199</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
+    <a class="book-preview">
+        <img class="book-preview-coverimage" src="/public/images/covers/aadu.jpg" />
+        <div class="book-preview-title-author">
+            <h1>Pathumayude Aadu</h1>
+            <p>By <span>Vaikam Muhammad Basheer</span></p>
+        </div>
+        <div class="book-preview-price-addtocart">
+            <p>₹151</p>
+            <img src="/public/images/addtocart.svg" title="add to cart" />
+        </div>
+    </a>
 </section>
 
 <?php include "./layouts/footer.php";?>
