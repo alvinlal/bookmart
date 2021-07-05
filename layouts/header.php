dd5f02a4fe280535909b156c5a077d87a131cbb1
Alvin Lal <alvinzzz2001@gmail.com>
2021-07-05 20:50:12 +0530

    heroku test

--- layouts/header.php
+++ layouts/header.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bookmart</title>
+    <title>Bookmart-test</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/public/css/index.css">
