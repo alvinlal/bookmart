6250bbd0e3e113cf14de1db82daed8dda97c878a
Alvin Lal <alvinzzz2001@gmail.com>
2021-07-06 22:58:31 +0530

    db test 5

--- db/connection.php
+++ db/connection.php
@@ -1,12 +1,16 @@
 <?php
 
-$dbInfo = getenv('DATABASE_URL');
+$dbhost = getenv("DB_HOST");
+$dbport = getenv("DB_PORT");
+$dbname = getenv("DB_NAME");
+$username = getenv("DB_USERNAME");
+$password = getenv("DB_PASSWORD");
 
-// phpinfo();
+$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};";
 
 $starttime = microtime();
 
-$pdo = new PDO($dbInfo);
+$pdo = new PDO($dsn, $username, $password);
 
 $stmt = $pdo->query('SHOW DATABASES;');
 
@@ -16,8 +20,6 @@ while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
 
 $endtime = microtime();
 
-$finaltime = $endtime - $starttime;
-
-echo "query took " . $finaltime;
+echo "query took " . $starttime - $endtime;
 
 ?>
