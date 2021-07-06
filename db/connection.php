8c993ff93f5fdecb446a0fec8add8511f4896034
Alvin Lal <alvinzzz2001@gmail.com>
2021-07-06 20:44:16 +0530

    db test 1

--- db/connection.php
+++ db/connection.php
@@ -1,7 +1,27 @@
 <?php
 
-$my_env_var = getenv('VAR_TEST');
+$dbhost = getenv("DB_HOST");
+$dbport = getenv("DB_PORT");
+$dbname = getenv("DB_NAME");
+$username = getenv("DB_USERNAME");
+$password = getenv("DB_PASSWORD");
 
-// echo $my_env_var;
+$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};";
+
+$starttime = microtime();
+
+$pdo = new PDO($dsn, $username, $password);
+
+$stmt = $pdo->query('SHOW DATABASES;');
+
+while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+	echo $row->Database . '<br>';
+}
+
+$endtime = microtime();
+
+$finaltime = $endtime - $starttime;
+
+echo "query took " . $finaltime;
 
 ?>
